37397bf85fa962d5e952ec26305abbb399e4bd85
Alberto Taiuti <alberto.taiuti@gmail.com>
2016-02-16 13:29:14 +0000

    Update cmakelists, add main.c and submodule szthreadpool

--- src/main.c
+++ src/main.c
@@ -1,15 +1,12 @@
 
 //#include "thread_pool.h"
-#include <iostream>
-#include <thread>
-#include <atomic>
-#include <pthread.h>
-#include <Windows.h>
-extern "C"
-{
-#include "thread_pool_c.h"
+//#include <iostream>
+//#include <thread>
+//#include <atomic>
+//#include <pthread.h>
+//#include <Windows.h>
 #include "dbg.h"
-}
+#include "thread_pool_c.h"
 
 int main() {
   
